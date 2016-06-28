acb937f009642d62de8bda8d11c5f9add7a5c840
rcliffo6 <cdravtx@gmail.com>
2016-06-27 17:57:46 -0700

    Create DNA.pm

---
+++ week04/DNA.pm
@@ -0,0 +1,24 @@
+package DNA;
+
+#use strict;
+use warnings;
+
+use Exporter 'import';
+our @EXPORT_OK = ( "DNA_sequence");
+
+
+  sub DNA_sequence {
+    my @bases = qw/ A C G T /;
+    my( $length , $random_length ) = @_;
+    my $sequence = "";
+
+    if ( $random_length ) {
+      $length = int( rand( $length ));
+    }
+
+    foreach ( 1 .. $length ) {
+        my $nucleotide = $bases[int(rand(4))];
+        $sequence = $sequence.$nucleotide;
+    }
+    return $sequence;
+
